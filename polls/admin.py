97b2fbbeed6ca96860c2318c01e31b5c0dacb834
gbstanley12 <gbstanley@mail.fhsu.edu>
2023-11-07 13:12:57 -0600

    Functional from tutorial

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
-from .models import Movie
+from .models import Question
 
-@admin.register(Movie)
-class MovieAdmin(admin.ModelAdmin):
-    list_display = ('title', 'release_date', 'grossing')
+admin.site.register(Question)
